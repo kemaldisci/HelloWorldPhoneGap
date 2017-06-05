cb280269dbbfb8429e6ceeeeb1f52edac2693c35
kemaldisci <kemaldisci@gmail.com>
2017-06-05 23:35:16 +0300

    Change

--- HelloWorldPhoneGap/www/js/index.js
+++ HelloWorldPhoneGap/www/js/index.js
@@ -18,48 +18,24 @@
  */
 
 
-var app = angular.module('myApp', ["ngResource","ngRoute","ngCookies"]);
-app.controller('mainCtrl', 
-  function($scope, $resource, $http, $httpParamSerializer, $cookies) {
-     
-    $scope.data = {
-        grant_type:"password", 
-        username: "", 
-        password: "", 
-        client_id: "clientIdPassword"
-    };
-    $scope.encoded = btoa("clientIdPassword:secret");
-     
-    $scope.login = function() {   
-        var req = {
-            method: 'POST',
-            url: "http://localhost:8080/spring-security-oauth-server/oauth/token",
-            headers: {
-                "Authorization": "Basic " + $scope.encoded,
-                "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
-            },
-            data: $httpParamSerializer($scope.data)
-        }
-        $http(req).then(function(data){
-            $http.defaults.headers.common.Authorization = 
-              'Bearer ' + data.data.access_token;
-            $cookies.put("access_token", data.data.access_token);
-            window.location.href="index";
-        });   
-   }    
-});
-
-
-var isLoginPage = window.location.href.indexOf("signin") != -1;
-if(isLoginPage){
-    if($cookies.get("access_token")){
-        window.location.href = "index";
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        window.document.location.href = 'index.html';
     }
-} else{
-    if($cookies.get("access_token")){
-        $http.defaults.headers.common.Authorization = 
-          'Bearer ' + $cookies.get("access_token");
-    } else{
-        window.location.href = "signin";
-    }
-}
+    // Update DOM on a Received Event
+};
