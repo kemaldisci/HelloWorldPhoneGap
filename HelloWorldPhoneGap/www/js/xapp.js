bfa2e81eb46baef7e06b4a4b21fee93b67117cb3
BaturOz <batuhanoztuna@gmail.com>
2017-06-07 21:41:50 +0300

    created service

--- HelloWorldPhoneGap/www/js/xapp.js
+++ HelloWorldPhoneGap/www/js/xapp.js
@@ -1,18 +1,32 @@
 var app = angular.module('xApp', []);
+app.service('CreatedService', function(){
+    this.CreateFunction = function (number) {
+        return number.toString(2);
+    }
 
-app.controller('xCtrl', function($scope,$http,$interval) {
-    $scope.myFunction = function() {
+});
+app.service('CreatedService2', function(){
+    this.CreateFunction2 = function(enc){
+        return enc.window.btoa(enc);
+
+    }
+});
+
+app.controller('xCtrl', function($scope,$http,$interval,CreatedService) {
+    $scope.number = CreatedService.CreateFunction(1453);
+    var str = "ss"
+    $scope.enc = enc.window.btoa(str);
+    $scope.time = new Date().toLocaleTimeString();
+    $interval(function () {
         $scope.time = new Date().toLocaleTimeString();
-        $interval(function () {
-            $scope.time = new Date().toLocaleTimeString();
-        }, 1000);
-        $http.get("https://www.w3schools.com/angular/customers.php").then(
-            function isSuccess(response) {
-                $scope.result = response.status
-                $scope.xdata = response.data.records;
-            }, 
-            function isError(response) {
-                $scope.result = response.status ;
-            });
-            $scope.count="Fixed";
-  }});
+    }, 1000);
+    $http.get("https://www.w3schools.com/angular/customers.php").then(
+        function isSuccess(response) {
+            $scope.result = response.status
+            $scope.xdata = response.data.records;
+        }, 
+        function isError(response) {
+            $scope.result = response.status ;
+        });
+        $scope.fixedValue="Fixed";
+    });
