8f4742a4d3b29e37b5820b3470d5c66b25561e33
BaturOz <batuhanoztuna@gmail.com>
2017-06-15 18:53:45 +0300

    signup control

--- HelloWorldPhoneGap/www/js/index.js
+++ HelloWorldPhoneGap/www/js/index.js
@@ -1,41 +1,31 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        window.document.location.href = 'index.html';
+var app = angular.module('index', []);
+app.service('indexService', function(){
+    this.encode = function (xcode){
+        return window.btoa(xcode);
     }
-    // Update DOM on a Received Event
-};
+});
+app.controller('indexCtrl', function($scope,$http,$interval,indexService) {
+    $scope.login = function(){
+        if ($scope.uid==null){
+            alert("Uid is null");
+            return ;
+        }
+        else if ($scope.pwd==null){
+            alert("pwd is null");
+            return ;
+        } 
+        else
+        $scope.login_error = "";    
+        $scope.u_id = "User ID : " +$scope.uid;
+        $scope.p_wd = "Encode password : "+indexService.encode($scope.pwd);
+        $scope.time = "Try time : "+new Date().toLocaleTimeString();
+        $http.get("http://localhost/").then(
+            function isSuccess(ResponseResult) {
+                $scope.ResponseResult = response.status
+                $scope.xdata = response.data.records;
+            }, 
+            function isError(response) {
+                $scope.ResponseResult = response.status ;
+            });
+            $scope.fixedValue="Fixed";
+    }});
---
+++ HelloWorldPhoneGap/www/js/signup.js
@@ -0,0 +1,8 @@
+var app = angular.module('signup', []);
+;
+app.controller('signupCtrl', function($scope) {
+    $scope.x1="Test js";
+        
+    
+    
+});
